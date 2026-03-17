524bad0b6e8de8d2f85a765106d62e2bd9458146
yuchansun <nicosun0515@gmail.com>
2026-03-17 15:25:21 +0800

    時間格式修正

--- 專題-react-version/api/db.py
+++ 專題-react-version/api/db.py
@@ -9,7 +9,7 @@ def get_conn():
         host=os.getenv("DB_HOST", "localhost"),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASS", ""),
-        db=os.getenv("DB_NAME", "triage"),
+        db=os.getenv("DB_NAME", "medical_triage_system"),
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=True,
